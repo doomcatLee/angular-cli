e111b41e9cafcda7ce80e0a5881c6934e27d50be
Alex Lee <doomcat9167@gmail.com>
2017-05-09 13:53:57 -0700

    add routing and dynamic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { WelcomeComponent } from './welcome/welcome.component';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
+import { AboutComponent } from './about/about.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AblumDetailComponent } from './ablum-detail/ablum-detail.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    AboutComponent,
+    MarketplaceComponent,
+    AblumDetailComponent,
+    AlbumDetailComponent
   ],
   imports: [
     BrowserModule,
