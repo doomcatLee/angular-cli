da0023cf2f2d7199161bcb1cfc677fc3a4a3c5f7
Alex Lee <doomcat9167@gmail.com>
2017-05-09 13:34:21 -0700

    add tempate structure using angular cli

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
